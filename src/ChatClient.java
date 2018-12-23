08df2b03a6d7c2a0ca28d823f528c51c28037175
AlanLiang <1114173646@qq.com>
2018-12-23 22:39:14 +0800

    verson0.2:           1.Client连接上Server;           2.Client发送一条信息到Server

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,25 +1,50 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class ChatClient extends Frame {
-    public void launchFrame() {
+    TextField tf = new TextField();
+    TextArea ta = new TextArea();
+
+    public void launchFrame() throws IOException {
         setTitle("chat");
         setLocation(100, 100);
-        setSize(800, 600);
-        setVisible(true);
+        setSize(600, 400);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+        ta.setFont(new Font("宋体", Font.PLAIN, 24));
+        tf.setFont(new Font("宋体", Font.PLAIN, 24));
+        add(ta, BorderLayout.NORTH);
+        add(tf, BorderLayout.SOUTH);
+        tf.addActionListener(new TFListener());
+        setVisible(true);
+        connect();
     }
 
+    private class TFListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String str = tf.getText();
+            ta.setText(str);
+            tf.setText("");
+        }
+    }
+    public void connect() throws IOException {
+        Socket s = new Socket("192.168.203.1", 8888);
+        System.out.println("connected");
+        DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+        dos.writeUTF("Hello!");
+    }
     public static void main(String[] args) throws IOException {
         new ChatClient().launchFrame();
-        Socket s=new Socket("192.168.203.1",8888);
-        System.out.println("connected!");
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,11 +1,14 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss=new ServerSocket(8888);
-        Socket s=ss.accept();
+        ServerSocket ss = new ServerSocket(8888);
+        Socket s = ss.accept();
         System.out.println("A client connected!");
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        System.out.println(dis.readUTF());
     }
 }
