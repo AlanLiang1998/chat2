c2aeab9626cc27859b5b64485755e39d670b61e6
AlanLiang <1114173646@qq.com>
2018-12-23 23:00:15 +0800

    version0.5:            1.多个Client通过GUI发送多条信息到Server

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -5,18 +5,42 @@ import java.net.Socket;
 import java.net.SocketException;
 
 public class ChatServer {
+    ServerSocket ss = null;
+
     public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(8888);
-        Socket s = ss.accept();
-        System.out.println("A client connected!");
-        DataInputStream dis = new DataInputStream(s.getInputStream());
+        new ChatServer().start();
+    }
+
+    public void start() throws IOException {
+        ss = new ServerSocket(8888);
         while (true) {
-            try {
-                String str = dis.readUTF();
-                System.out.println(str);
-            }catch (SocketException e){
-                System.out.println("A client closed");
-                break;
+            Socket s = ss.accept();
+            Client c = new Client(s);
+            new Thread(c).start();
+            System.out.println("A client connected!");
+        }
+    }
+
+    private class Client implements Runnable {
+        Socket s = null;
+        DataInputStream dis = null;
+
+        public Client(Socket s) throws IOException {
+            this.s = s;
+            dis = new DataInputStream(s.getInputStream());
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                } catch (SocketException e) {
+                    System.out.println("A client closed");
+                    break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
